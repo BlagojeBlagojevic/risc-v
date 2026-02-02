258b3dcc280a38b605b5937c4fe480cb63e1ad50
Blagoje Blagojevic <blagojeblagojevic562@gmail.com>
2026-02-02 21:51:37 +0100

    Add files via upload

---
+++ test.c
@@ -0,0 +1,41 @@
+#define UART_STORE 0x10000000
+#define UART_READ  0x10000001
+
+void putchar(char c);
+void puts_simple();  
+int main() {
+    puts_simple();
+    
+    // Write 'a' to UART as requested
+    volatile char *ptr = (volatile char *)UART_STORE;
+    volatile char *read = (volatile char *)UART_READ;
+    
+    *ptr = 'a';
+    
+    __asm__("nop");
+    
+    char dummy = *read;
+    return 0;
+}
+
+void putchar(char c) {
+    volatile char *ptr = (volatile char *)UART_STORE;
+    *ptr = c;
+}
+
+void puts_simple() {
+    putchar('H');
+    putchar('e');
+    putchar('l');
+    putchar('l');
+    putchar('o');
+    putchar(' ');
+    putchar('W');
+    putchar('o');
+    putchar('r');
+    putchar('l');
+    putchar('d');
+    putchar('!');
+    putchar('\n');
+}
+
